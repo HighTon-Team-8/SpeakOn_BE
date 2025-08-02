45976350fea2bd68f455f4112a4398a7ee12fe53
박태수 <taesupark0504@gmail.com>
2025-08-02 18:28:02 +0900

    chore :: gradle setting

---
+++ src/main/java/com/example/team8be/global/error/exception/ErrorCode.java
@@ -0,0 +1,42 @@
+package com.example.firststep_server.global.error.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+@Getter
+@AllArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorCode {
+
+    //jwt
+    EXPIRED_TOKEN(401, "만료된 토큰입니다."),
+    INVALID_TOKEN(401, "검증 되지 않은 토큰 입니다."),
+    REFRESH_TOKEN_NOT_FOUND(404, "일치 하는 RefreshToken이 존재 하지 않습니다."),
+    INVALID_ROLE(401,"유효 하지 않은 역할입니다."),
+
+    //user
+    USER_NOT_FOUND(404, "해당 유저가 존재 하지 않습니다."),
+    USER_MISMATCH(401, "유저가 일치 하지 않습니다."),
+    PASSWORD_MISMATCH(401, "비밀 번호가 일치 하지 않습니다."),
+    INVALID_USER(401, "유효 하지 않은 사용자입니다."),
+    USER_EXIST(401, "유저가 이미 존재합니다."),
+
+    //s3
+    IMAGE_NOT_FOUND(404, "이미지를 찾을 수 없음"),
+    FAILED_UPLOAD(1001, "업로드 실패"),
+    FAILED_DELETE(1002, "삭제 실패"),
+
+    //problem
+    PROBLEM_NOT_FOUND(404, "Problem Not Found"),
+
+    //completion
+    COMPLETION_NOT_FOUND(404, "Completion Not Found"),
+
+    // general
+    BAD_REQUEST(400, "프론트 탓"),
+    INTERNAL_SERVER_ERROR(500, "서버 탓");
+
+
+    private final int statusCode;
+    private final String ErrorMessage;
+}
