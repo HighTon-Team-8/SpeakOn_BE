67bdf0e9fd96148e0316ac1647f2c8fa8a0a484a
박태수 <taesupark0504@gmail.com>
2025-08-02 22:17:19 +0900

    feat :: baseTimeIdEntity

---
+++ src/main/java/com/example/team8be/global/entity/BaseTimeIdEntity.java
@@ -0,0 +1,22 @@
+package com.example.team8be.global.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeIdEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
