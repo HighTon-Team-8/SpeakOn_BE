a6525e271e287e2d7d593975bf8815577086f347
박태수 <taesupark0504@gmail.com>
2025-08-03 05:24:58 +0900

    chore :: redis

--- src/main/java/com/example/team8be/global/redis/RedisConfig.java
+++ src/main/java/com/example/team8be/global/redis/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.example.team8be.global.redis;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -18,11 +19,17 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 )
 @RequiredArgsConstructor
 public class RedisConfig {
-    private final RedisProperties properties;
+    @Value("${redis.host}")
+    private String host;
+    
+    @Value("${redis.port}")
+    private Integer port;
+
+
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(properties.getHost(), properties.getPort());
+        return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
