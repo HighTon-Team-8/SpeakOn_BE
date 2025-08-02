cf743a0070d9ff082d45e4a74ed4495730be0c4a
박태수 <taesupark0504@gmail.com>
2025-08-02 22:18:24 +0900

    feat :: presentationController

---
+++ src/main/java/com/example/team8be/domain/presentation/domain/presentation/PresentationController.java
@@ -0,0 +1,11 @@
+package com.example.team8be.domain.presentation.domain.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/presentation")
+@RequiredArgsConstructor
+public class PresentationController {
+}
