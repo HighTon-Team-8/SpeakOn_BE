88b271f8a60658586d0b4f5a648f4d82ef5d4ae1
박태수 <taesupark0504@gmail.com>
2025-08-02 18:11:03 +0900

    chore :: 기본 세팅

---
+++ src/main/java/com/example/team8be/Team8BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.team8be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team8BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team8BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/team8be/Team8BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team8be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team8BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
