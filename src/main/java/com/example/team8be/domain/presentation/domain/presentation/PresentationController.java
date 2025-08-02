e76d6cc7788507e87fa38aa44f07d5ae2d114466
박태수 <taesupark0504@gmail.com>
2025-08-02 22:33:45 +0900

    feat :: upload api

--- src/main/java/com/example/team8be/domain/presentation/domain/presentation/PresentationController.java
+++ src/main/java/com/example/team8be/domain/presentation/domain/presentation/PresentationController.java
@@ -1,11 +1,21 @@
 package com.example.team8be.domain.presentation.domain.presentation;
 
+import com.example.team8be.domain.presentation.domain.Presentation;
+import com.example.team8be.domain.presentation.service.UploadPresentationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/presentation")
 @RequiredArgsConstructor
 public class PresentationController {
+    private final UploadPresentationService presentationUploadService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/upload")
+    public Presentation upload(@RequestPart(value = "file") MultipartFile file) {
+        return presentationUploadService.execute(file);
+    }
 }
