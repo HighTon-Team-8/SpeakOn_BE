760c25b28d574d7d9d061bc25a88ee087a4c2a51
박태수 <taesupark0504@gmail.com>
2025-08-02 22:33:58 +0900

    feat :: queryPresentationService

---
+++ src/main/java/com/example/team8be/domain/presentation/service/QueryPresentationService.java
@@ -0,0 +1,26 @@
+package com.example.team8be.domain.presentation.service;
+
+import com.example.team8be.domain.presentation.domain.Presentation;
+import com.example.team8be.domain.presentation.domain.presentation.dto.PresentationResponse;
+import com.example.team8be.domain.presentation.domain.repository.PresentationRepository;
+import com.example.team8be.domain.presentation.exception.PresentationNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class QueryPresentationService {
+    private final PresentationRepository presentationRepository;
+
+    public PresentationResponse execute(Long presentationId) {
+        Presentation presentation = presentationRepository.findById(presentationId)
+                .orElseThrow(() -> PresentationNotFoundException.EXCEPTION);
+
+        return PresentationResponse.builder()
+                .file_url(presentation.getFile_url())
+                .transcript(presentation.getTranscript())
+                .deliveryScore(presentation.getDeliveryScore())
+                .feedback(presentation.getFeedback())
+                .build();
+    }
+}
