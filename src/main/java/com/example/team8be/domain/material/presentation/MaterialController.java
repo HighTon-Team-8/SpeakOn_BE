ff6b42ddcbe8d56f6af8703eed819554e92297fa
박태수 <taesupark0504@gmail.com>
2025-08-03 00:15:00 +0900

    feat :: query api

--- src/main/java/com/example/team8be/domain/material/presentation/MaterialController.java
+++ src/main/java/com/example/team8be/domain/material/presentation/MaterialController.java
@@ -2,6 +2,8 @@ package com.example.team8be.domain.material.presentation;
 
 import com.example.team8be.domain.material.domain.Material;
 import com.example.team8be.domain.material.presentation.dto.request.MaterialRequest;
+import com.example.team8be.domain.material.presentation.dto.response.MaterialResponse;
+import com.example.team8be.domain.material.service.QueryMaterialService;
 import com.example.team8be.domain.material.service.UploadMaterialService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -14,11 +16,18 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class MaterialController {
     private final UploadMaterialService uploadMaterialService;
+    private final QueryMaterialService queryMaterialService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/upload")
     public Material uploadMaterial(@RequestPart(name = "file") MultipartFile file,
                                    @RequestPart(name = "request") @Valid MaterialRequest request) {
         return uploadMaterialService.execute(file, request);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("{id}")
+    public MaterialResponse queryMaterial(@PathVariable Long id) {
+        return queryMaterialService.execute(id);
     }
 }
