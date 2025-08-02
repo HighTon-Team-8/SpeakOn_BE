3b1b3ab5bfff177a405909d86940e3a51f25f3d8
박태수 <taesupark0504@gmail.com>
2025-08-02 22:41:27 +0900

    feat :: queryAllPresentationService

---
+++ src/main/java/com/example/team8be/domain/presentation/service/QueryAllPresentationService.java
@@ -0,0 +1,23 @@
+package com.example.team8be.domain.presentation.service;
+
+import com.example.team8be.domain.presentation.domain.presentation.dto.PresentationResponse;
+import com.example.team8be.domain.presentation.domain.repository.PresentationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QueryAllPresentationService {
+    private final PresentationRepository presentationRepository;
+
+    @Transactional(readOnly = true)
+    public List<PresentationResponse> execute() {
+        return presentationRepository.findAll()
+                .stream()
+                .map(PresentationResponse::new)
+                .toList();
+    }
+}
