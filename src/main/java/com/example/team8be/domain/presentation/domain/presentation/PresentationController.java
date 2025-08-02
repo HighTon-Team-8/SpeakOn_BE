624bdda1455e8ce26de13aeb484e101fdb4d53bc
박태수 <taesupark0504@gmail.com>
2025-08-02 22:41:01 +0900

    feat :: query api

--- src/main/java/com/example/team8be/domain/presentation/domain/presentation/PresentationController.java
+++ src/main/java/com/example/team8be/domain/presentation/domain/presentation/PresentationController.java
@@ -1,6 +1,8 @@
 package com.example.team8be.domain.presentation.domain.presentation;
 
 import com.example.team8be.domain.presentation.domain.Presentation;
+import com.example.team8be.domain.presentation.domain.presentation.dto.PresentationResponse;
+import com.example.team8be.domain.presentation.service.QueryPresentationService;
 import com.example.team8be.domain.presentation.service.UploadPresentationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,10 +14,17 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class PresentationController {
     private final UploadPresentationService presentationUploadService;
+    private final QueryPresentationService queryPresentationService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/upload")
     public Presentation upload(@RequestPart(value = "file") MultipartFile file) {
         return presentationUploadService.execute(file);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("{id}")
+    public PresentationResponse queryPresentation(@PathVariable Long id) {
+        return queryPresentationService.execute(id);
     }
 }
