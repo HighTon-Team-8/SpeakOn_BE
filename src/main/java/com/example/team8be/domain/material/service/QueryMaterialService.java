74b6bdb78cf34f69a2f928bbbed5d0826dc5e2bc
박태수 <taesupark0504@gmail.com>
2025-08-03 00:13:39 +0900

    feat :: QuerymaterialService

---
+++ src/main/java/com/example/team8be/domain/material/service/QueryMaterialService.java
@@ -0,0 +1,36 @@
+package com.example.team8be.domain.material.service;
+
+import com.example.team8be.domain.material.domain.Material;
+import com.example.team8be.domain.material.domain.repository.MaterialRepository;
+import com.example.team8be.domain.material.exception.MaterialNotFoundException;
+import com.example.team8be.domain.material.presentation.dto.response.MaterialResponse;
+import com.example.team8be.domain.script.domain.Script;
+import com.example.team8be.domain.script.domain.repository.ScriptRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QueryMaterialService {
+    private final MaterialRepository materialRepository;
+    private final ScriptRepository scriptRepository;
+
+    @Transactional(readOnly = true)
+    public MaterialResponse execute(Long materialId) {
+        Material material = materialRepository.findById(materialId)
+                .orElseThrow(() -> MaterialNotFoundException.EXCEPTION);
+
+        List<Script> scripts = scriptRepository.findByMaterialIdOrderBySlideNumberAsc(materialId);
+
+        return MaterialResponse.builder()
+                .id(material.getId())
+                .title(material.getTitle())
+                .script(scripts)
+                .createdAt(material.getCreatedAt())
+                .build();
+
+    }
+}
