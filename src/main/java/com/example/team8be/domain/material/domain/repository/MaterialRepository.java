6ce3744709c0d36790639c995c3c2b63fe33f931
박태수 <taesupark0504@gmail.com>
2025-08-02 23:50:31 +0900

    feat :: materialRepository

---
+++ src/main/java/com/example/team8be/domain/material/domain/repository/MaterialRepository.java
@@ -0,0 +1,9 @@
+package com.example.team8be.domain.material.domain.repository;
+
+import com.example.team8be.domain.material.domain.Material;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MaterialRepository extends JpaRepository<Material, Long> {
+}
